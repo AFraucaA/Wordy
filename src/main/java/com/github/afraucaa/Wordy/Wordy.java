aa9de1f6b3efd1c5418c98febbb4e53bdd916332
AFraucaA <adfrauca@gmail.com>
2025-02-27 13:07:40 -0500

    Reorganized files a bit. Code runs now.

---
+++ src/main/java/com/github/afraucaa/Wordy/Wordy.java
@@ -0,0 +1,9 @@
+package com.github.afraucaa.Wordy;
+
+
+class Wordy {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        WordyEngine engine = new WordyEngine();
+    }
+}
---
+++ src/main/java/com/github/afraucaa/Wordy/WordyEngine.java
@@ -0,0 +1,7 @@
+package com.github.afraucaa.Wordy;
+
+class WordyEngine {
+    public WordyEngine() {
+        System.out.println("Good morning Vietnam!");
+    }
+}
